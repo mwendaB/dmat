e3d6101bc5b2a1921c932e02967089b480673a03
Brian mwenda <87479232+mwendaB@users.noreply.github.com>
2024-10-14 08:31:26 +0000

    add in. gnh

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: '/dmat',
+    assetPrefix: '/dmat/',
+  };
+  
+  export default nextConfig;
+  
